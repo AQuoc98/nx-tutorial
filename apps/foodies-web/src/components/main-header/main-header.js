448b066b56e0d5728c2cee6083b2eded5cde747a
Quốc Anh <quocanh@Quocs-MacBook-Pro.local>
2024-02-22 00:08:59 +0700

    init commit

---
+++ apps/foodies-web/src/components/main-header/main-header.js
@@ -0,0 +1,42 @@
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import logoImg from '@/assets/logo.png'
+import classes from './main-header.module.scss'
+import { usePathname } from 'next/navigation'
+import MainHeaderBackground from './main-header-background'
+
+export default function MainHeader() {
+  const path = usePathname()
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link
+                href="/meals"
+                className={path.startsWith('/meals') ? classes.active : undefined}
+              >
+                Browse Meals
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/community"
+                className={path === '/community' ? classes.active : undefined}
+              >
+                Foodies Community
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
---
+++ apps/styling-structure/src/app/app.js
@@ -0,0 +1,9 @@
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="styling-structure" />
+    </div>
+  );
+}
+export default App;
